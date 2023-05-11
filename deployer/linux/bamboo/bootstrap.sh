7a74affbb487e9a38aa8fb61aca5f77b6aee47e5
Guillermo Chacon <guillermoch@gmail.com>
2023-05-11 17:09:44 -0600

    Update bootstrap.sh

--- deployer/linux/bamboo/bootstrap.sh
+++ deployer/linux/bamboo/bootstrap.sh
@@ -1,29 +1,3 @@
-################################################################################
-##
-## Common bootstrap for all bamboo jobs:
-##   1) Git checkout to build server
-##   2) Salt provision build server
-##
-## In Bamboo, the task should be a shell script file task with the following
-## arguments (sorted alphabetically):
-##
-##   --build_server    ${bamboo.build_server}
-##   --domain          ${bamboo.domain}
-##   --domain_username ${bamboo.domain_username}
-##   --domain_password ${bamboo.domain_password}
-##   --full            ${bamboo.full}
-##   --git_remote      ${bamboo.git_remote}
-##   --git_ref         ${bamboo.planRepository.1.revision}
-##   --http_proxy      ${bamboo.http_proxy}
-##   --installers_path ${bamboo.installers_path}
-##   --repository_root ${bamboo.repository_root}
-##   --skip_git_pull   ${bamboo.skip_git_pull}
-##   --tunnel_via      ${bamboo.tunnel_via}
-##
-################################################################################
-
-#source "$(dirname "${0}")/../utilities.sh"
-#preserve_script_log /var/log/deploy/build.log
 
 skip_git_pull=false
 extra_args=""
@@ -48,4 +22,4 @@ do
     shift
 done
 
-echo "Checking out repository onto buildserver:" "$build_server"
+echo "Checking out repository onto buildserver:"
