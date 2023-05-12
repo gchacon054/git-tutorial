4dae90a7f703a9fc15e8ed142ae1d2f523abdffb
Guillermo Chacon <guillermoch@gmail.com>
2023-05-12 11:02:02 -0600

    Update bootstrap.sh

--- deployer/linux/bamboo/bootstrap.sh
+++ deployer/linux/bamboo/bootstrap.sh
@@ -9,7 +9,47 @@ do
         --build_server)
             build_server="${2}"
             shift ;;
-
+        --domain)
+            domain="${2}"
+            shift ;;
+        --domain_username)
+            domain_username="${2}"
+            shift ;;
+        --domain_password)
+            domain_password="${2}"
+            shift ;;
+        --full)
+            if [[ "${2}" = true ]];
+            then
+                extra_args="${extra_args} -full"
+            fi
+            shift ;;
+        --git_remote)
+            git_remote="${2}"
+            shift ;;
+        --git_ref)
+            git_ref="${2}"
+            shift ;;
+        --http_proxy)
+            if [[ "${2}" != --* && -n "${2}" ]];
+            then
+                http_proxy="${2}"
+                shift
+            fi
+            ;;
+        --installers_path)
+            installers_path="${2}"
+            shift ;;
+        --repository_root)
+            repository_root="${2}"
+            shift ;;
+        --skip_git_pull)
+            if [[ "${2}" != --* && -n "${2}" ]];
+            then
+                skip_git_pull="${2}"
+                shift
+            fi
+            ;;
         --tunnel_via)
             if [[ "${2}" != --* && -n "${2}" ]];
             then
@@ -18,6 +58,15 @@ do
             fi
             ;;
         *)
+            if [[ "${1}" == --template_* ]];
+            then
+                extra_args="${extra_args} -${1#*template_} ${2}"
+                shift
+            else
+                echo "Unknown argument: ${1}"
+                exit 1
+            fi
+            ;;
     esac
     shift
 done
